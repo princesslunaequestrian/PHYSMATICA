df14392e79fc73213aab26ac4f46f493885dd5f9
princesslunaequestrian <backgroundpony@outlook.com>
2020-12-07 03:01:02 +0300

    Rewritten using canvas
    Fuck setInterval
    Praise RFA

--- Pendulum/JS/pendulum.js
+++ Pendulum/JS/pendulum.js
@@ -1,114 +1,198 @@
 console.log("start");
 
 // Initialising ineractive objects
-//var ball = document.getElementById('ball');
-var spring = document.getElementById('spring-img');
-var springheight = parseFloat(spring.getAttribute('height', 30)); //getting standart height of the spring
-var springdiv = document.getElementById('spring') //spring container resizes also
+
+var canv = document.getElementById('canvas');
+
+    canv.width = 375;
+    canv.height = 600;
+
+var ctx = canv.getContext('2d');
+
 var button = document.getElementById('start-button');
+
 var mass = document.getElementById('mass-input');
+
 var stif = document.getElementById('stif-input');
 
-//console.log(spring);
-//console.log(parseFloat(spring.getAttribute('height')));
-//console.log(springdiv);
+var anim = false; //global animation tracker
+var RAF = null;
+var scale = 100;
 
-//global animation tracker
-let anim = false;
+var N = 13; //Number of nodes
 
-//amplitude may be changed
-A = 0.25;
+//Physics
 
-//default period, may be ditched
-T = 1000;
+var G = 9.8,
+    M = 1,
+    K = 1,
+    F = M*G,
+    a = F/M,
+    v = 0,
+    Eq = M*G/K,
+    delta = 0;
 
 
+//Creating spring nodes
+var pen_length = 300;
+
+function Nodes(length)
+{
+    var n = [];
+
+    n.push([canv.width/2, 75]);
+    n.push([canv.width/2, 75+10]);
+
+    for (let i = 0; i < N; i++)
+    {
+        var nx = i % 2 == 0 ? canv.width/2 + 35 : canv.width/2 - 35;
+        var ny = 75+10+10/300*length + i*20/300*length;
+        n.push([nx, ny]);
+    }
+    
+    var ly = n[n.length-1][1];
+
+    n.push([canv.width/2, ly+10]);
+    n.push([canv.width/2, ly+20]);
+    n.push([canv.width/2, ly+50]);
+
+    return n;
+
+}
+
+//Drawing pendulum stand
+
+var nodes = Nodes(pen_length);
+
+function drawStand(nodes)
+{
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canv.width, canv.height);
+
+    ctx.fillStyle = 'brown';
+    ctx.fillRect(0, 0, 375, 75);
+
+    ctx.fillStyle = '#ED930C';
+    ctx.fillRect(0, 75, 50, canv.height - 75);
+    ctx.fillRect(canv.width-50, 75, 50, canv.height - 75);
+
+    for (let i = 0; i < nodes.length-2; i++)
+    {
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.lineTo(nodes[i][0], nodes[i][1]);
+        ctx.lineTo(nodes[i+1][0], nodes[i+1][1]);
+        ctx.stroke();
+        console.log('drawing line');
+    }
+    
+    ctx.fillStyle = 'magenta';
+    ax = canv.width/2;
+    ay = nodes[nodes.length-1][1];
+    ctx.moveTo(ax, ay);
+    ctx.arc(ax, ay, 30, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+
+drawStand(nodes);
 
 
-button.onclick = function() {
+function step(time)
+{
+    dt = (Date.now() - time)/100000000000000;
+    F = -delta*K/scale + M*G;
+    a = F/M;
+    v += a*dt;
+    delta += v*dt*scale;
+    if (delta <= -pen_length)
+    {
+        alert('Вы уничтожили пружину! Теперь покупайте новую!');
+        anim = false;
+    }
 
-    if (!anim) {
+    if (anim)
+    {
+        time = Date.now()
+        console.log('-------');
+        console.log(dt);
+        console.log(F);
+        console.log(a);
+        console.log(v);
+        console.log(delta);
+        console.log('-------');
+
+        drawStand(Nodes(pen_length+delta));
+        var RAF = requestAnimationFrame(step);
+    }
+    else
+    {
+        console.log('stopping animation');
+        drawStand(Nodes(pen_length));
+        button.textContent = 'СТАРТ';
+        a = 0;
+        v = 0;
+        F = M*G;
+        delta = 0;
+    }
+}
+
+function onClick()
+{
+
+    if (anim)
+    {
+        cancelAnimationFrame(RAF);
+        anim = false;
+    }
+
+    else
+    {
+        console.log('starting animation');
 
         anim = true;
 
+        button.textContent = 'СТОП';
 
-        //Parsing user input
-        var M = parseFloat(mass.value);
-        var K = parseFloat(stif.value);
+        var startTime = Date.now();
 
-        //console.log(K);
-        //console.log(typeof(K));
+        M = parseFloat(mass.value);
+        K = parseFloat(stif.value);
 
-
-        //Checking for <0 or NaNs
-        if (M <= 0 || M != M|| K <= 0 || K != K) { //For some reason Number.isNaN is not working, spitting "Number.isNan is not a function"
+        if (M <= 0 || M != M|| K <= 0 || K != K)
+        { 
+            //For some reason Number.isNaN is not working,
+            //spitting "Number.isNan is not a function"
 
             //Breaking out
             console.log("User durachok");
             alert("Масса или коэффициент жёсткости должны быть больше 0!")
+
             anim = false;
+
+            button.textContent = 'СТАРТ';
+
             return;
         }
-
-        button.textContent = 'СТОП';
-
-        //Calculating period
-        T = 2*Math.PI*Math.sqrt(M/K)*1000;
-        console.log("Period: " + String(T) + " sec");
-
-
-        startTime = Date.now();
-
-        //console.log('Starting animation at '+Date.now().toString());
-
-        //Running timer
-        var timer = setInterval(function() {
-            
-            timePassed = Date.now() - startTime;
-
-            animate(timePassed);
-
-            if (!anim) {
-
-                //Resetting everything back as the animation has been stopped by the user
-                clearInterval(timer);
-                console.log('Animation stopped');
-                spring.setAttribute('height', springheight);
-                springdiv.setAttribute('height', springheight);
-                button.textContent = 'СТАРТ';
-            }
-
-        }, 20)
-
         
+        step(startTime);
 
     }
+}
 
-    else {
-        console.log('Stopping animation');
-
-        anim = false;
+function onChange()
+{
+    M = parseFloat(mass.value);
+    K = parseFloat(stif.value);
+    Eq = M*G/K;
+    if (!anim)
+    {
+        drawStand(Nodes(pen_length));
     }
-
-    return;
-
 }
 
 
 
-function animate(time) {
 
-    //Calculating phase and the coefficient of spring scaling (contraction/strtching)
 
-    let phase = time*2*Math.PI/T;
-
-    let coef = (1 - A*Math.sin(phase));
-
-    newheight = coef*springheight;
-
-    spring.setAttribute('height', newheight);
-
-    springdiv.setAttribute('height', newheight);
-
-    
-}
